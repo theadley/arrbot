923315a32512ad4a6af3814af10537d3800bd00a
Timothy Headley <theadley@jhb.dvt.co.za>
2024-06-24 13:23:37 +0200

    feat: Fix build

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18-alpine
+FROM node:20.9.0-alpine
 
 # Create app directory
 WORKDIR /usr/src/app
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build . -t headleytm/arr-bot
+docker build . -t headleytm/arr-bot --platform linux/x86_64
 docker tag headleytm/arr-bot headleytm/arr-bot:latest
 docker image push headleytm/arr-bot
